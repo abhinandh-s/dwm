e2f9d8bf321885c38cd738bec57f700dcf2320ef
Abhinandh S <ugabhi@proton.me>
2025-04-20 10:15:34 +0530

    chore: test 1

--- flake.nix
+++ flake.nix
@@ -5,17 +5,21 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
-    packages.x86_64-linux.default = let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-    in
-      pkgs.callPackage ./default.nix {
-        configFile = null;
-      };
+  outputs = { nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+
+    # Import the DWM package and allow for the configFile to be passed
+    dwm = import ./default.nix {
+      inherit pkgs;
+      configFile = null;  # You can override this in your NixOS configuration or through the flake itself
+    };
+  in {
+    # Explicitly define default package for flake consumers
+    packages.x86_64-linux.default = dwm;
+
+    # Or alternatively, if you prefer to define it via defaultPackage
+    defaultPackage.x86_64-linux = dwm;
   };
 }
+
